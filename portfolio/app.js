79bb4fcd6fdacfb74e297cf66987d3a7287de788
hannasharifi <103643879+hannasharifi@users.noreply.github.com>
2023-06-22 18:59:22 +0200

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,7 @@
+// JavaScript code for header animation
+window.addEventListener('DOMContentLoaded', () => {
+    const headerText = document.getElementById('headerText');
+  
+    // Trigger the animation once the page has loaded
+    headerText.style.animationPlayState = 'running';
+  });
