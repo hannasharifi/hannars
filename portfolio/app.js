fe4ef816e8def1bf6bc214f2baf24966fe7c1006
hannasharifi <103643879+hannasharifi@users.noreply.github.com>
2023-07-24 00:38:00 +0200

    Delete portfolio directory

--- portfolio/app.js
+++
@@ -1,7 +0,0 @@
-// JavaScript code for header animation
-window.addEventListener('DOMContentLoaded', () => {
-    const headerText = document.getElementById('headerText');
-  
-    // Trigger the animation once the page has loaded
-    headerText.style.animationPlayState = 'running';
-  });
